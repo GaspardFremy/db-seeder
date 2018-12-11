e1b380832f5da4b78f6539e7f4f264defcfda821
GaspardFremy <gaspardfremy123@gmail.com>
2018-12-12 00:43:33 +0100

    add sql

---
+++ users.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
